50c4992b2c3a0cb921fee6071a6eea72575da377
Pratikpradhan640 <pratikpradhan640@gmail.com>
2025-08-10 02:22:42 +0530

    Initial commit

---
+++ src/assets/data.js
@@ -0,0 +1,44 @@
+export const data=
+[
+{
+    question: "Who is the father of Computers?",
+     option1:"James Gosling",
+    option2:"Charles Babbage",
+    option3:"Bjarne Stroustrup",
+    option4:"Dennis Ritchie",
+    answer:2,
+},
+{
+    question: "Which of the following is the brain of the computer?",
+     option1:"Central Processing Unit",
+    option2:"Memory",
+    option3:"Arithmetic and Logic Unit",
+    option4:"Control Unit",
+    answer:1,
+},
+{
+    question: "Which of the following is the smallest unit of data in a computer? ",
+     option1:" Bit",
+    option2:"KB",
+    option3:"Nibble",
+    option4:"Byte",
+    answer:1,
+},
+{
+    question: "Which of the following cannot be used to modify the data in a database?",
+     option1:"insert",
+    option2:"update",
+    option3:"drop",
+    option4:"delete",
+    answer:3,
+},
+{
+    question: " Which data structure is used for implementing recursion?",
+     option1:"List",
+    option2:"Stack",
+    option3:"Queue",
+    option4:"Array",
+    answer:2,
+},
+
+];
